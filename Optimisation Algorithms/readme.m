868e364abe03f5e6802c5d690763f1f8983c6d9a
SANSKAR PANDEY <78846892+PANDEYSANSKAR@users.noreply.github.com>
2022-05-31 14:44:27 +0530

    Update readme.m

--- Optimisation Algorithms/readme.m
+++ Optimisation Algorithms/readme.m
@@ -1,1 +1,9 @@
 
+# Optimization Methods
+
+Until now, you've always used Gradient Descent to update the parameters and minimize the cost. In this notebook, you'll gain skills with some more advanced optimization methods that can speed up learning and perhaps even get you to a better final value for the cost function. Having a good optimization algorithm can be the difference between waiting days vs. just a few hours to get a good result. 
+
+By the end of this notebook, you'll be able to: 
+
+* Apply optimization methods such as (Stochastic) Gradient Descent, Momentum, RMSProp and Adam
+* Use random minibatches to accelerate convergence and improve optimization
